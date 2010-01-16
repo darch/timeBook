3d9d20d26588e43214ebe1acc1052912a9f277ba
Shusaku Yokota <blueberrycandle@gmail.com>
2010-01-16 20:06:24 +0900

    change output process

--- Calendar.rb
+++ Calendar.rb
@@ -1,58 +1,26 @@
 #!/usr/local/bin/ruby
-require File.dirname(__FILE__) + "/Dba"
-load 'config.rb', true
 
 # define Calendar Class
 class Calendar
-# define weekday display name
-WeekName = [
-'Sun',
-'Mon',
-'Tue',
-'Wed',
-'Thu',
-'Fri',
-'Sat',
-]
-
-# define weekday display color
-WeekColor = [
-'#ff0000',
-'#000000',
-'#000000',
-'#000000',
-'#000000',
-'#000000',
-'#0000ff',
-]
-
-# define today color
-TodayColor = '#ffff00'
 
 # define initialize method
 def initialize(year, month)
 	@year = year.to_i
 	@month = month.to_i
 	@wday = []
-	@daydata = []
 
 	# if month is invalied show raise error message
 	if(@month < 1 ) || (12 < @month)
-		raise "Month Error"
+		raise "Month Error(#{@month})"
 	end # if
 
 	# if year is invalied or too future show raise error message
 	if(@year < 1) || (2037 < @year)
-		raise "Year Error"
+		raise "Year Error(#{@year})"
 	end # if
-
-	# get today
-	nowday = Time.local(Time.now.year, Time.now.month, Time.now.day, 0, 0, 0)
 
 	(1..31).each do |day|
 		itsday = Time.local(@year, @month, day, 0, 0, 0)
-
-		@daydata[day] = 'today' if nowday == itsday
 
 		if day > 28 && itsday.month != @month
 			@wday[day] = nil
@@ -61,90 +29,28 @@ def initialize(year, month)
 		end # if
 	end # each
 
-	@dba = Dba.new($config[:server], $config[:database], $config[:user], $config[:passwd])
-
 end # def initialize
 
-def get_wday(day)
-	day = day.to_i
-	return WeekName[@wday[day]]
-end # get_wday
+# define get_calendar
+def get_calendar
+	# 日付情報を格納する配列を宣言
+	cal_data = Array.new(0)
 
-# define output html
-def html_print
-	# define before and next year
-	before_year = @year
-	next_year = @year
-
-	# calc before and next month
-	before_month = @month - 1
-	next_month = @month + 1
-
-	if before_month == 0
-		before_month = 12
-		before_year -= 1
-	end # if
-
-	if next_month == 13
-		next_month = 1
-		next_year = next_year += 1
-	end # if
-
-	# getting data from database
-	rows = @dba.selectMonth(fix_digit(@year, 4), fix_digit(@month, 2))
-	rows_ite = 0
-
-	print_data = "<form name=\"goto_input\" action=\"./input.cgi\" method=\"POST\">\n"
-	print_data += "<input type=\"hidden\" id=\"year\" name=\"year\" value=\"#{@year}\"/>\n"
-	print_data += "<input type=\"hidden\" id=\"month\" name=\"month\" value=\"#{@month}\"/>\n"
-	print_data += "<input type=\"hidden\" id=\"day\" name=\"day\" value=\"\"/>\n"
-	print_data += "<table class=\"time_book\">\n"
-	print_data += "<caption>\n"
-	print_data += "<a href=\"./list.cgi?year=#{fix_digit(before_year, 4)}&month=#{fix_digit(before_month, 2)}\">&lt;</a>\n"
-	print_data += "#{fix_digit(@year, 4)}&nbsp;/&nbsp;#{fix_digit(@month, 2)}\n"
-	print_data += "<a href=\"./list.cgi?year=#{fix_digit(next_year, 4)}&month=#{fix_digit(next_month, 2)}\">&gt;</a>\n"
-	print_data += "</caption>\n"
-
-	# print header
-	print_data += "<tr>\n"
-	print_data += "<th class=\"date_header\">date</th>\n"
-	print_data += "<th class=\"day_header\">day</th>\n"
-	print_data += "<th class=\"start_header\">start</th>\n"
-	print_data += "<th class=\"end_header\">end</th>\n"
-	print_data += "<th class=\"memo_header\">memo</th>\n"
-	print_data += "</tr>\n"
-	# print daydata
 	(1..@wday.length - 1).each do |day|
+		# 曜日がnilになったら月が終わりなのでbreak
 		if @wday[day] == nil
 			break
 		end # if
-		startTime = ""
-		endTime = ""
-		memo = ""
-		if rows.size != 0 and rows[rows_ite][0] == "#{fix_digit(@year, 4)}#{fix_digit(@month, 2)}#{fix_digit(day, 2)}"
-			startTime = rows[rows_ite][1]
-			endTime = rows[rows_ite][2]
-			memo = rows[rows_ite][3]
-			if rows.size - 1 > rows_ite
-				rows_ite += 1
-			end # if
-		end # if
-		print_data += "<tr>\n"
-		print_data += "<td class=\"date_data #{WeekName[@wday[day]]}\"><a href=\"javascript:goto_Input(#{fix_digit(day, 2)});\" target=\"_self\">#{fix_digit(day, 2)}</a></td>\n"
-		print_data += "<td class=\"day_data #{WeekName[@wday[day]]}\">#{WeekName[@wday[day]]}</td>\n"
-		print_data += "<td class=\"start_data\">#{startTime}</td>\n"
-		print_data += "<td class=\"end_data\">#{endTime}</td>\n"
-		print_data += "<td class=\"memo_data\">#{memo}</td>\n"
-		print_data += "</tr>\n"
+
+		# 日付と曜日番号をひとつの配列にする
+		day_data = ["#{day}", @wday[day]]
+
+		# 日付情報に日付と曜日番号の配列を追加
+		cal_data << day_data
 	end # each
 
-	print_data += "</table>\n"
-	print_data += "</form>\n"
-	end # def print_html
+	# 日付情報を返却
+	return cal_data
+end # def get_calendar
 
-	# 文字列と桁数を受けとり、桁数分0で埋めた文字列を返す。
-	def fix_digit(str, digit)
-		str = "%0#{digit}d" % str.to_s
-		return str
-	end # def fix_digit
 end # class Calendar
--- list.cgi
+++ list.cgi
@@ -5,22 +5,73 @@ require File.dirname(__FILE__) + "/Dba"
 require "cgi-lib"
 load "config.rb", true
 
+# define weekday display name<
+WeekName = [
+'Sun',
+'Mon',
+'Tue',
+'Wed',
+'Thu',
+'Fri',
+'Sat',
+]
+
+# 文字列と桁数を受けとり、桁数分0で埋めた文字列を返す。
+def fix_digit(str, digit)
+	str = "%0#{digit}d" % str.to_s
+	return str
+end # def fix_digit
+
 input = CGI.new
-year = input['year'] || Time.now.year.to_s
-month = input['month'] || Time.now.month.to_s
-day = input['day'] || Time.now.day.to_s
-if month.length == 1
-	month = "0" + month
-end
-if day.length == 1
-	day = "0" + day
+year = input['year'] || Time.now.year
+month = input['month'] || Time.now.month
+day = input['day'] || Time.now.day
+
+year = year.to_i
+month = month.to_i
+day = day.to_i
+
+# 次の月と、前の月を設定
+if month == 1
+	next_month = month + 1
+	prev_month = 12
+	next_year = year
+	prev_year = year - 1
+elsif month == 12
+	next_month = 1
+	prev_month = month - 1
+	next_year = year + 1
+	prev_year = year
+else
+	next_month = month + 1
+	prev_month = month - 1
+	next_year = year
+	prev_year = year
 end
 
-if input['command'] == "submit"
+begin
+	# カレンダー情報を取得
+	calendar = Calendar.new(year, month)
+	cal_data = calendar.get_calendar
+
+	# データベースに接続
 	dba = Dba.new($config[:server], $config[:database], $config[:user], $config[:passwd])
-	dba.inputDate("#{year}#{month}#{day}", "#{input['start']}", "#{input['end']}", "#{input['memo']}")
+
+	# 入力情報がある場合はデータベースに登録
+	if input['command'] == "submit"
+		dba.inputDate("#{fix_digit(year, 4)}#{fix_digit(month, 2)}#{fix_digit(day, 2)}", "#{input['start']}", "#{input['end']}", "#{input['memo']}")
+	end
+
+	# 入力済み情報の取得
+	rows = dba.selectMonth(fix_digit(year, 4), fix_digit(month, 2))
+	rows_ite = 0
+
+	# データベースから切断
 	dba.disconnect
+rescue
+	print "#{$!}\n"
 end
+
 
 print "Content-type: text/html;charset=UTF-8\n\n"
 print "<html>\n"
@@ -31,11 +82,50 @@ print "<link rel=\"stylesheet\" type=\"text/css\" href=\"timeBook.css\"/>\n"
 print "<script type=\"text/javascript\" src=\"timeBook.js\"></script>"
 print "</head>\n"
 print "<body>\n"
+print "<form name=\"goto_input\" action=\"./input.cgi\" method=\"POST\">\n"
+print "<input type=\"hidden\" id=\"year\" name=\"year\" value=\"#{year}\"/>\n"
+print "<input type=\"hidden\" id=\"month\" name=\"month\" value=\"#{month}\"/>\n"
+print "<input type=\"hidden\" id=\"day\" name=\"day\" value=\"\"/>\n"
+print "<table class=\"time_book\">\n"
+print "<caption>\n"
+print "<a href=\"./list.cgi?year=#{fix_digit(prev_year, 4)}&month=#{fix_digit(prev_month, 2)}\">&lt;</a>\n"
+print "#{fix_digit(year, 4)}&nbsp;/&nbsp;#{fix_digit(month, 2)}\n"
+print "<a href=\"./list.cgi?year=#{fix_digit(next_year, 4)}&month=#{fix_digit(next_month, 2)}\">&gt;</a>\n"
+print "</caption>\n"
+print "<tr>\n"
+print "<th class=\"date_header\">date</th>\n"
+print "<th class=\"day_header\">day</th>\n"
+print "<th class=\"start_header\">start</th>\n"
+print "<th class=\"end_header\">end</th>\n"
+print "<th class=\"memo_header\">memo</th>\n"
+print "</tr>\n"
 begin
-	calendar = Calendar.new(year, month)
-	print "#{calendar.html_print}\n"
+	cal_data.each do |day_data|
+		day = day_data[0]
+		wday = WeekName[day_data[1]]
+		startTime = ""
+		endTime = ""
+		memo = ""
+		if rows.size != 0 and rows[rows_ite][0] == "#{fix_digit(year, 4)}#{fix_digit(month, 2)}#{fix_digit(day, 2)}"
+			startTime = rows[rows_ite][1]
+			endTime = rows[rows_ite][2]
+			memo = rows[rows_ite][3]
+			if rows.size - 1 > rows_ite
+				rows_ite += 1
+			end # if
+		end # if
+		print "<tr>\n"
+		print "<td class=\"date_data #{wday}\"><a href=\"javascript:goto_Input(#{fix_digit(day, 2)});\" target=\"_self\">#{fix_digit(day, 2)}</a></td>\n"
+		print "<td class=\"day_data #{wday}\">#{wday}</td>\n"
+		print "<td class=\"start_data\">#{startTime}</td>\n"
+		print "<td class=\"end_data\">#{endTime}</td>\n"
+		print "<td class=\"memo_data\">#{memo}</td>\n"
+		print "</tr>\n"
+	end
 rescue
 	print "#{$!}\n"
 end
+print "</table>\n"
+print "</form>\n"
 print "</body>\n"
 print "</html>\n"
